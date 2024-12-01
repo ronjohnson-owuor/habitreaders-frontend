ce99780349f9dd21c0e574e34b7997f3c349f22a
Ronjohnson Owuor <ronjohnsonowuor83@gmail.com>
2024-12-01 22:45:26 +0300

    working on the landing page

--- app/components/Onboard.tsx
+++ app/components/Onboard.tsx
@@ -4,9 +4,12 @@ function Onboard() {
   return (
     <div className='w-full my-10 bg-customForeground-light min-h-[400px]'>
         <div>
-            
+
         </div>
-        <div></div>
+        <div>
+            <h1>your user journey</h1>
+            <p>become a habitual reader by following these easy steps.The best experiece is on the other side,You just need to get started</p>
+        </div>
     </div>
   )
 }
